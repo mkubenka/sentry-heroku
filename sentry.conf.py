6ac81a692f7a637c66b31b5a9171d8e25aaf9b3b
Michal Kubenka <mkubenka@gmail.com>
2014-04-23 10:55:04 +0200

    Reorganization.

--- sentry.conf.py
+++ sentry.conf.py
@@ -1,15 +1,13 @@
-import logging
+import dj_database_url
+
 import os
-import sys
+import logging
 
 from sentry.conf.server import *
 
-ROOT = os.path.dirname(__file__)
-
-sys.path.append(ROOT)
-
-import dj_database_url
-DATABASES = {'default': dj_database_url.config()}
+DATABASES = {
+    'default': dj_database_url.config(default="postgres://localhost"),
+}
 
 
 # Sentry configuration
@@ -20,14 +18,14 @@ SENTRY_KEY = os.environ.get('SENTRY_KEY')
 # Set this to false to require authentication
 SENTRY_PUBLIC = False
 
+SENTRY_URL_PREFIX = os.environ.get('SENTRY_URL_PREFIX', '')
+
 SENTRY_WEB_HOST = '0.0.0.0'
-SENTRY_WEB_PORT = int(os.environ.get('PORT', 9000))
+SENTRY_WEB_PORT = os.environ.get('PORT', 80)
 SENTRY_WEB_OPTIONS = {
     'workers': 3,
-    'worker_class': 'gevent',
+	'worker_class': 'gevent',
 }
-
-SENTRY_URL_PREFIX = os.environ.get('SENTRY_URL_PREFIX', '')
 
 
 # Email configuration
@@ -53,7 +51,6 @@ EMAIL_SUBJECT_PREFIX = '[Sentry] '
 
 # The reply-to email address for outgoing mail.
 SERVER_EMAIL = os.environ.get('SERVER_EMAIL', 'root@localhost')
-
 
 # Security
 # --------
@@ -111,44 +108,3 @@ INSTALLED_APPS += ('django_bcrypt',)
 #
 # The hash is also migrated when ``BCRYPT_ROUNDS`` changes.
 BCRYPT_MIGRATE = True
-
-
-# Social Auth
-# -----------
-
-SOCIAL_AUTH_CREATE_USERS = 'SOCIAL_AUTH_CREATE_USERS' in os.environ
-
-
-# Twitter
-# -------
-TWITTER_CONSUMER_KEY = os.environ.get('TWITTER_CONSUMER_KEY')
-TWITTER_CONSUMER_SECRET = os.environ.get('TWITTER_CONSUMER_SECRET')
-
-
-# Facebook
-# --------
-
-FACEBOOK_APP_ID = os.environ.get('FACEBOOK_APP_ID')
-FACEBOOK_API_SECRET = os.environ.get('FACEBOOK_API_SECRET')
-
-
-# Google
-# ------
-
-GOOGLE_OAUTH2_CLIENT_ID = os.environ.get('GOOGLE_OAUTH2_CLIENT_ID')
-GOOGLE_OAUTH2_CLIENT_SECRET = os.environ.get('GOOGLE_OAUTH2_CLIENT_SECRET')
-
-
-# GitHub
-# ------
-
-GITHUB_APP_ID = os.environ.get('GITHUB_APP_ID')
-GITHUB_API_SECRET = os.environ.get('GITHUB_API_SECRET')
-GITHUB_EXTENDED_PERMISSIONS = ['repo']
-
-
-# Bitbucket
-# ---------
-
-BITBUCKET_CONSUMER_KEY = os.environ.get('BITBUCKET_CONSUMER_KEY')
-BITBUCKET_CONSUMER_SECRET = os.environ.get('BITBUCKET_CONSUMER_SECRET')
